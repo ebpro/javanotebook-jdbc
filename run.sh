4aa95a4b97c53f97a9210337f22764cf22256bad
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2021-01-30 15:25:11 +0100

    initial release.

---
+++ run.sh
@@ -0,0 +1,13 @@
+docker run \
+	--name jupyterjava \
+        --rm \
+	--volume $PWD/notebooks:/notebooks \
+        --volume $PWD/src:/src \
+        --volume $PWD/codeserver:/codeserver \
+	--env NOTEBOOK_SRC_SUBDIR=${PWD##*/} \
+        --publish 8888:8888 \
+        --env PUID=$UID \
+      	--env PGID=$GID \
+       	--env SUDO_PASSWORD=secret \
+        --volume ~/.m2:/home/user/.m2 \
+        brunoe/jupyterjava:develop
